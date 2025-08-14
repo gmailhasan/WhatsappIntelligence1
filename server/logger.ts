44e9834a13b07cf110e46fd4e2fa1f0f28baecf3
gmailhasan <md.hasan@gmail.com>
2025-08-13 22:50:12 -0700

    logger updated

--- server/logger.ts
+++ server/logger.ts
@@ -21,8 +21,14 @@ class Logger {
         level: 'info',
         format: winston.format.combine(
           winston.format.timestamp(),
-          winston.format.printf(({ timestamp, level, message }) => {
-            return `${timestamp} [${level}]: ${message}`;
+          winston.format.printf((info) => {
+            const { timestamp, level, message, ...meta } = info;
+            let metaString = '';
+            // If meta contains additional properties, stringify them
+            if (Object.keys(meta).length > 0) {
+              metaString = ' ' + JSON.stringify(meta);
+            }
+            return `${timestamp} [${level}]: ${message}${metaString}`;
           })
         ),
         transports: [
